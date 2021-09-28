403d3b907c206081d7c7019c59b81e0355189bce
Morgan Rosenkranz <morgan@alloyenterprises.xyz>
2021-09-28 14:26:03 -0400

    moved count inc and dec to Ref and got rid of pub for fn b/c not needed

--- src/containers/ref.zig
+++ src/containers/ref.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const expect = std.testing.expect;
 
 /// Create a new shared pointer 'factory' of type T
 pub fn Ref(comptime T: type) type {
@@ -10,25 +11,6 @@ pub fn Ref(comptime T: type) type {
         val: T,
         /// number of strong references to this pointer
         count: usize,
-
-        /// increase the pointer count
-        inline fn inc(ptr: *Ptr) void {
-            // load the value
-            var v = @atomicLoad(usize, &ptr.*.count, .Aquire);
-            // compare to see if the value has changed since loading
-            while (@cmpxchgWeak(usize, &ptr.*.count, v + 1, .Release, .Monotonic)) |nv| {
-                v = nv;
-            }
-        }
-
-        /// decrease the pointer count
-        inline fn dec(ptr: *Ptr) void {
-            var v = @atomicLoad(usize, &ptr.*.count, .Aquire);
-            // compare to see if the value has changed since loading
-            while (@cmpxchgWeak(usize, &ptr.*.count, if (v == 0) v else v - 1, .Release, .Monotonic)) |nv| {
-                v = nv;
-            }
-        }
     };
 
     // Returns a strong reference to the inner value
@@ -39,7 +21,7 @@ pub fn Ref(comptime T: type) type {
         inner: *Ptr,
 
         /// allocates a new strong reference given the value v
-        pub fn new(allocator: *Allocator, v: T) !Self {
+        fn new(allocator: *Allocator, v: T) !Self {
             // allocate the inner
             const n_inner = try allocator.create(Ptr);
             n_inner.* = Ptr{
@@ -53,34 +35,97 @@ pub fn Ref(comptime T: type) type {
         }
 
         /// Destroy the allocated inner counter and value
-        pub fn deinit(self: *Self, allocator: *Allocator) void {
-            allocator.destroy(self.inner);
+        /// if this is the only reference.
+        /// Otherwise, we just decrement the counter
+        fn deinit(self: Self, allocator: *Allocator) void {
+            const c = dec(self.inner);
+            if (c == 1) {
+                allocator.destroy(self.inner);
+            }
         }
 
         /// create a new strong reference to the value shared in here
-        pub fn clone(self: *Self) !Self {
-            self.inner.inc();
+        fn clone(self: Self) Self {
+            _ = inc(self.inner);
             return Self{
                 .inner = self.inner,
             };
         }
 
         /// returns a const pointer to the value stored in this Ref
-        pub fn ptr(self: *Self) *const T {
+        fn ptr(self: *Self) *const T {
             return &self.inner.*.val;
         }
 
         /// returns an unsafe, mutable pointer to this value.
         /// this should only be used if there is one owner of
         /// this reference
-        pub fn rawPtr(self: *Self) *T {
+        fn rawPtr(self: *Self) *T {
             return &self.inner.*.val;
+        }
+
+        /// Get the number of references to this data
+        inline fn count(self: Self) usize {
+            return @atomicLoad(usize, &self.inner.*.count, .SeqCst);
+        }
+
+        /// increase the pointer count and return the previous value
+        inline fn inc(p: *Ptr) usize {
+            // load the value
+            var v = @atomicLoad(usize, &p.*.count, .Acquire);
+            // compare to see if the value has changed since loading
+            while (@cmpxchgWeak(usize, &p.*.count, v, v + 1, .Release, .Monotonic)) |nv| {
+                v = nv;
+            }
+            return v;
+        }
+
+        /// decrease the pointer count and return the previous value
+        inline fn dec(p: *Ptr) usize {
+            var v = @atomicLoad(usize, &p.*.count, .Acquire);
+            // compare to see if the value has changed since loading
+            while (@cmpxchgWeak(usize, &p.*.count, v, if (v == 0) v else v - 1, .Release, .Monotonic)) |nv| {
+                v = nv;
+            }
+            return v;
         }
     };
 }
 
 test "init" {
     const allocator = std.testing.allocator;
-    var p = try Ref(u8).new(allocator, 5);
+    const p = try Ref(u8).new(allocator, 5);
     defer p.deinit(allocator);
+
+    // test that the count is 1
+    try expect(p.inner.*.count == 1);
+}
+
+test "clone and deinit" {
+    const allocator = std.testing.allocator;
+    const p = try Ref(u8).new(allocator, 5);
+    defer p.deinit(allocator);
+
+    try expect(p.inner.*.count == 1);
+
+    var p2 = p.clone();
+    // check count
+    try expect(p.inner.*.count == 2);
+    try expect(p2.inner.*.count == 2);
+    try expect(p2.inner.*.val == 5);
+    try expect(p.inner.*.val == 5);
+
+    p2.deinit(allocator);
+
+    // the count should now b 1
+    try expect(p.inner.*.count == 1);
+}
+
+test "count" {
+    const allocator = std.testing.allocator;
+    const p = try Ref(u8).new(allocator, 5);
+    defer p.deinit(allocator);
+
+    try expect(p.inner.*.count == p.count());
+    try expect(p.count() == 1);
 }
